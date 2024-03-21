9c494fa63ffc0eaab26d910ec3c2eba645436da5
Kathy <kathyauma72@gmail.com>
2024-03-21 21:01:48 +0300

    secomd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thisismeapp/screens/home_page.dart';
 import 'package:thisismeapp/screens/login.dart';
+import 'package:thisismeapp/screens/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(),
+      home: const Register(),
     );
   }
 }
---
+++ lib/screens/register.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 122, 116, 116),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Hey There Welcome!'),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your name',
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey))),
+                ),
+              ),
+              SizedBox(height: 25,),
+               Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration( 
+                    labelText: "Enter your email",
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey))),
+                ),
+              ),
+               Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText:'Enter your password' ,
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey))),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
